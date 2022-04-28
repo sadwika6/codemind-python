590d7cbbf3785e868fc759ae31d48f4af70d5666
sadwika6 <97425809+sadwika6@users.noreply.github.com>
2022-04-28 19:05:01 +0530

    def ugly_1(n):     if(n==1):         return 1     if(n<=0):         return 0     if n%2==0:         return ugly_1(n//2)     if n%3==0:         return ugly_1(n//3)     if n%5==0:         return ugly_1(n//5)     return 0; n=int(input()) if(ugly_1(n)):     print('Ugly Number') else:     print('Not Ugly Number')

---
+++ Ugly_number.py
@@ -0,0 +1,17 @@
+def ugly_1(n):
+    if(n==1):
+        return 1
+    if(n<=0):
+        return 0
+    if n%2==0:
+        return ugly_1(n//2)
+    if n%3==0:
+        return ugly_1(n//3)
+    if n%5==0:
+        return ugly_1(n//5)
+    return 0;
+n=int(input())
+if(ugly_1(n)):
+    print('Ugly Number')
+else:
+    print('Not Ugly Number')
