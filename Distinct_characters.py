edd45d24fc20f625804a75baa78c407caa91a7f2
sadwika6 <97425809+sadwika6@users.noreply.github.com>
2022-06-19 18:58:37 +0530

    s=input() s=s.lower() a=sorted(set(s)) z='' z=z.join(a) z=z.replace(" ","") print(z)

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,10 +1,7 @@
 s=input()
-c=''
 s=s.lower()
-for i in s:
-    if i!=' ' and s.count(i)==1:
-        c+=i
-x=sorted(c)
-y=''
-y=y.join(x)
-print(y)
+a=sorted(set(s))
+z=''
+z=z.join(a)
+z=z.replace(" ","")
+print(z)
