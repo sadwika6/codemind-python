afc9dfde2b8b2f3f03febec8989409fcec293752
sadwika6 <97425809+sadwika6@users.noreply.github.com>
2022-07-03 16:14:07 +0530

    s1=input() s1=s1.lower() s2=input() s2=s2.lower() for i in s1:     if i not in s2:         print(False)         break else:     print(True)

--- Anagrams.py
+++ Anagrams.py
@@ -1,8 +1,10 @@
 s1=input()
-s2=input()
 s1=s1.lower()
+s2=input()
 s2=s2.lower()
-if sorted(s1)==sorted(s2):
-    print("True")
+for i in s1:
+    if i not in s2:
+        print(False)
+        break
 else:
-    print("False")
+    print(True)
