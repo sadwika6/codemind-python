1dcb3094db9f4e0204bf8bafae26c63a011cc44d
sadwika6 <97425809+sadwika6@users.noreply.github.com>
2022-07-12 15:28:42 +0530

    s1=input() s2=input() s1=s1.lower() s2=s2.lower() k=[] for i in s1:     if i not in s2 and i not in k and i!=' ':         k.append(i) for i in s2:     if i not in s1 and i not in k and i!=' ':         k.append(i) k.sort() if k==[]:     print(-1) else:     k=''.join(k)     print(k)

---
+++ non_common_characters.py
@@ -0,0 +1,17 @@
+s1=input()
+s2=input()
+s1=s1.lower()
+s2=s2.lower()
+k=[]
+for i in s1:
+    if i not in s2 and i not in k and i!=' ':
+        k.append(i)
+for i in s2:
+    if i not in s1 and i not in k and i!=' ':
+        k.append(i)
+k.sort()
+if k==[]:
+    print(-1)
+else:
+    k=''.join(k)
+    print(k)
