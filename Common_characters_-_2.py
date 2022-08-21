8e72a362c3dbd3df0902f2b1a43f91cc6c03f7ce
sadwika6 <97425809+sadwika6@users.noreply.github.com>
2022-08-21 14:30:43 +0530

    a=input() a=a.lower() s=a.split() k="" y=0 for i in s:     for j in i:         y=0         for x in s:             if j in x:                 y=1             else:                 y=0                 break         if y==1:             k+=j     break if len(k)>0:     print(len(k)) else:     print(0)

---
+++ Common_characters_-_2.py
@@ -0,0 +1,21 @@
+a=input()
+a=a.lower()
+s=a.split()
+k=""
+y=0
+for i in s:
+    for j in i:
+        y=0
+        for x in s:
+            if j in x:
+                y=1
+            else:
+                y=0
+                break
+        if y==1:
+            k+=j
+    break
+if len(k)>0:
+    print(len(k))
+else:
+    print(0)
