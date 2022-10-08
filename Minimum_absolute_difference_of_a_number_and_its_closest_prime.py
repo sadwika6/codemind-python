d5a5215f90e5385d44292bd027228364b765e085
sadwika6 <97425809+sadwika6@users.noreply.github.com>
2022-10-08 10:54:27 +0530

    def prime(n):     for i in range(2,(n//2)+1):         if n%i==0:             return False     return True n=int(input()) for i in range(n,1,-1):     if prime(i):         x=i         break for j in range(n,(n*n)+1,1):     if prime(j):         y=j         break if n-x<=y-n:     print(n-x) else:     print(y-n)

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -0,0 +1,18 @@
+def prime(n):
+    for i in range(2,(n//2)+1):
+        if n%i==0:
+            return False
+    return True
+n=int(input())
+for i in range(n,1,-1):
+    if prime(i):
+        x=i
+        break
+for j in range(n,(n*n)+1,1):
+    if prime(j):
+        y=j
+        break
+if n-x<=y-n:
+    print(n-x)
+else:
+    print(y-n)
